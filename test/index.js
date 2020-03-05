7b0e932784301dea79a21660194e28e0f138bd07
INEX07 <46562212+INEX07@users.noreply.github.com>
2020-03-05 11:39:34 +0545

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 const ytdl = require("../index");
-const Discord = require("discord.js");
+const Discord = require("discord.js"); // discord.js v11.5.1
 const client = new Discord.Client();
 
 client.on("ready", () => {
@@ -10,11 +10,11 @@ client.on("message", msg => {
     if (msg.author.bot || !msg.guild) return;
     if (msg.content === "??play") {
         if (!msg.member.voiceChannel) return msg.channel.send("You're not in a voice channel?");
-        let input = ytdl("https://youtube.com/watch?v=7wtfhZwyrcc", {
+        let input = ytdl("https://youtube.com/watch?v=ERu6jh_1gR0", {
             filter: "audioonly",
             quality: "highestaudio",
             highWaterMark: 1 << 25,
-            passArgs: ['-af', 'equalizer=f=40:width_type=h:width=50:g=15'] // custom ffmpeg args
+            encoderArgs: ['-af', 'equalizer=f=40:width_type=h:width=50:g=10']
         });
         
         msg.member.voiceChannel.join()
