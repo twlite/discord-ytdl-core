3e33e39e3ccd06ebdbac5c70f1ab4eec79180bcc
INEX07 <46562212+INEX07@users.noreply.github.com>
2020-04-12 18:33:51 +0545

    Update example.js

--- example.js
+++ example.js
@@ -3,28 +3,28 @@ const Discord = require("discord.js"); // discord.js v11.5.1
 const client = new Discord.Client();
 
 client.on("ready", () => {
-    console.log("boom")
+    console.log("boom");
 });
 
 client.on("message", msg => {
     if (msg.author.bot || !msg.guild) return;
     if (msg.content === "??play") {
-        if (!msg.member.voiceChannel) return msg.channel.send("You're not in a voice channel?");
+        if (!msg.member.voice.channel) return msg.channel.send("You're not in a voice channel?");
         let input = ytdl("https://youtube.com/watch?v=7wtfhZwyrcc", {
             filter: "audioonly",
-            quality: "highestaudio",
-            highWaterMark: 1 << 25,
             encoderArgs: ['-af', 'equalizer=f=40:width_type=h:width=50:g=15']
         });
 
-        msg.member.voiceChannel.join()
+        msg.member.voice.channel.join()
             .then(connection => {
-                connection.playOpusStream(input,
+                connection.play(input,
                     {
-                        highWaterMark: 1,
-                        bitrate: 320000
+                        type: "opus"
                     })
             })
+            .on("finish", () => {
+                msg.member.voice.channel.leave();
+            });
     }
 });
 
