7a5d2be17d9555ebfb9e4dfefc3e0d5d132e177d
INEX07 <46562212+INEX07@users.noreply.github.com>
2020-04-15 12:10:30 +0545

    updated example

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 const ytdl = require("../index");
-const Discord = require("discord.js"); // discord.js v11.5.1
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
@@ -9,15 +9,15 @@ client.on("ready", () => {
 client.on("message", msg => {
     if (msg.author.bot || !msg.guild) return;
     if (msg.content === "??play") {
-        if (!msg.member.voiceChannel) return msg.channel.send("You're not in a voice channel?");
+        if (!msg.member.voice.channel) return msg.channel.send("You're not in a voice channel?");
         let input = ytdl("https://youtube.com/watch?v=ERu6jh_1gR0", {
             filter: "audioonly",
             encoderArgs: ['-af', 'equalizer=f=40:width_type=h:width=50:g=10']
         });
         
-        msg.member.voiceChannel.join()
+        msg.member.voice.channel.join()
         .then(connection => {
-            connection.playOpusStream(input)
+            connection.play(input, { type: "opus" })
         })
     }
 });
