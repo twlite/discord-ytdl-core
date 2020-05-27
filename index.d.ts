f04d5e257d50f12ffb0fe7e36082c0e1b68911c4
Androz2091 <androz2091@gmail.com>
2020-05-27 20:50:35 +0200

    Remove compiled files

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-import ytdl, { downloadOptions } from "ytdl-core";
-import prism from "prism-media";
-interface CreateOpusStreamOptions extends downloadOptions {
-    seek?: number;
-    encoderArgs?: any[];
-}
-declare const DiscordYTDLCore: ((url: string, options: CreateOpusStreamOptions) => prism.opus.Encoder) & typeof ytdl;
-export = DiscordYTDLCore;
